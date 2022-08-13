9e3cd76242e22efbdfc8d1de1e2c2aef4a9878d2
Divya gavandi <49062060+gavandivya@users.noreply.github.com>
2022-08-14 02:29:45 +0530

    fix:Handled Negative Quantity

--- index.js
+++ index.js
@@ -12,26 +12,38 @@ function showMessage(message) {
 function ProfitLoss() {
   const initialPriceNo = Number(initialPrice.value);
   const currentPriceNo = Number(currentPrice.value);
-  const quantityNo = Number(quantity.value);
+  let quantityNo = Number(quantity.value);
   if(initialPriceNo && currentPriceNo && quantityNo){
-    if (initialPriceNo > currentPriceNo) {
+    if(quantityNo >= 1)
+    { 
+      if (initialPriceNo > currentPriceNo) {
         const Loss = (initialPriceNo - currentPriceNo) * quantityNo;
         LossPercentage = (Loss / initialPriceNo) * 100;
         showMessage(`Hey, the loss is ${Loss} and percent is ${LossPercentage.toFixed(2)}%`);
         document.body.className = "lossClass";
-  } else if (initialPriceNo < currentPriceNo) {
-        const Profit = (currentPriceNo - initialPriceNo) * quantityNo;
-        ProPercentage = (Profit / initialPriceNo) * 100;
-        showMessage(`Hey, the profit is ${Profit} and percent is ${ProPercentage.toFixed(2)}%`);
-        document.body.className = "profitClass";
-  } else {
-        showMessage(`No gain no loss`);
-        document.body.className = "NoGainClass";
-        }
+        output.style.color="white";
+        } 
+        else if (initialPriceNo < currentPriceNo) {
+              const Profit = (currentPriceNo - initialPriceNo) * quantityNo;
+              ProPercentage = (Profit / initialPriceNo) * 100;
+              showMessage(`Hey, the profit is ${Profit} and percent is ${ProPercentage.toFixed(2)}%`);
+              document.body.className = "profitClass";
+              output.style.color="white";
+        } 
+        else {
+              showMessage(`No gain no loss`);
+              document.body.className = "NoGainClass";
+              output.style.color="black";
+              }
+      }
+      else{
+            showMessage("Please fill valid quantity");
+          }
   }
   else{
     showMessage("Please fill all the details");
     output.style.color="red";
+    document.body.className = "NoGainClass";
   }
 
 }
